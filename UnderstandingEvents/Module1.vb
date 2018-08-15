62f04f5613345c4ff7340690d7914e3f4ea92a56
martynaku <36759922+martynaku@users.noreply.github.com>
2018-08-15 10:57:42 +0100

    Add project files.

---
+++ UnderstandingEvents/Module1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Timers
+
+Module Module1
+
+    Sub Main()
+
+        Dim timer As New Timers.Timer(1000)
+
+        AddHandler timer.Elapsed, AddressOf Module1.HandleTheTimer1
+        AddHandler timer.Elapsed, AddressOf Module1.HandleTheTimer2
+
+        timer.Start()
+        Console.WriteLine("Press enter to stop handling the event twice.")
+        Console.ReadLine()
+        timer.Stop()
+
+        RemoveHandler timer.Elapsed, AddressOf Module1.HandleTheTimer2
+
+        timer.Start()
+        Console.ReadLine()
+        timer.Stop()
+
+    End Sub
+
+    Private Sub HandleTheTimer2(sender As Object, e As ElapsedEventArgs)
+        Console.WriteLine("HandleTheTimer2 Event: {0:HH:mm:ss.fff}", e.SignalTime)
+    End Sub
+
+    Private Sub HandleTheTimer1(sender As Object, e As ElapsedEventArgs)
+        Console.WriteLine("HandleTheTimer1 Event: {0:HH:mm:ss.fff}", e.SignalTime)
+    End Sub
+End Module
---
+++ UnderstandingEvents/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("UnderstandingEvents")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UnderstandingEvents")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d8f47854-9919-4113-b1d2-746e44b1df89")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
